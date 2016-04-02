35b25bf7d511c517e8c85cda0a8def039bf94cc8
Adrian Li <li.adrianmc@gmail.com>
2016-04-02 17:40:35 +0800

    bold function added

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {editorState: EditorState.createEmpty()};
     this.onChange = (editorState) => this.setState({editorState});
+    this.handleKeyCommand = (command) => this._handleKeyCommand(command);
+  }
+
+  _handleKeyCommand(command) {
+    const {editorState} = this.state;
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return true;
+    }
+    return false;
+  }
+
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
   }
 
   render() {
@@ -13,8 +28,13 @@ export default class App extends React.Component {
     return (
       <div id="content">
         <h1>Draft Editor</h1>
+        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
         <div className="editor">
-          <Editor editorState={editorState} onChange={this.onChange} />
+          <Editor
+            editorState={editorState}
+            onChange={this.onChange}
+            handleKeyCommand={this.handleKeyCommand}
+          />
         </div>
       </div>
     );
